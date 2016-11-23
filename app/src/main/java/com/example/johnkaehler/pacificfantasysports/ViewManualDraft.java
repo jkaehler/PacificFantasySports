b58ecb0d40d6759d125da7f03c744327011006db
buttslapper <jfranny92@aol.com>
2016-11-23 15:51:55 -0800

    Created several activities to reflect commissioner's abilities to draft manually

---
+++ app/src/main/java/com/example/johnkaehler/pacificfantasysports/ViewManualDraft.java
@@ -0,0 +1,13 @@
+package com.example.johnkaehler.pacificfantasysports;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewManualDraft extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_manual_draft);
+    }
+}
